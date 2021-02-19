5f49f84ac6ed4c7d3d29fda7533a47e2b446e58d
Laurent Bois <laurent.bois@beta.gouv.fr>
2021-02-19 17:16:55 +0100

    link missions to cards

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ Bonne journée,
   })
 })
 
-app.get("/mission-sante", (req, res) => {
+app.get("/mission-sante/:id", (req, res) => {
   const missionTitle = "Missions santé"
   const subject = `Je postule à la mission ${missionTitle}`
   const body = `Bonjour,
@@ -95,6 +95,18 @@ Je suis intéressé.e par la mission de ${missionTitle}. Pourriez-vous m'en dire
 
 Bonne journée,
 `
+  let i = 0;
+  let offre = null;
+
+  console.log(req.params.id)
+  content.offres.forEach(function (_offre) {
+    console.log(_offre.id)
+    if (_offre.id == req.params.id) {
+      offre = _offre;
+
+    }
+    i++;
+  });
 
   const applyLink = process.env.APPLY_URL_SANTE
     ? process.env.APPLY_URL_SANTE
@@ -108,7 +120,7 @@ Bonne journée,
     missionTitle,
     applyLink,
     makeMailto,
-    content
+    offre
   })
 
 
